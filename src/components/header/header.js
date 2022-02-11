0de1aab95276b10b70aba46b3229b92b4cd1fe5e
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-02-11 18:12:05 +0700

    Delete header.js
    header.js > header.ts

--- src/components/header/header.js
+++
@@ -1,8 +0,0 @@
-let burger = document.getElementById('burger');
-let links = document.getElementById('links');
-let body = document.body;
-
-burger.addEventListener('click', function() {
-   links.classList.toggle('_active');
-   body.classList.toggle('_blockSwipe')
-})
