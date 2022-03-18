60700cc8ecf923e9f3abb44549d0ec010499f3c3
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-03-18 17:53:29 +0300

    build: change extension to .ts

--- src/js/room-details.ts
+++ src/js/room-details.ts
@@ -1,5 +1,5 @@
 import '../components/comments/comments'
-import '../components//to-book/to-book'
+import '../components/to-book/to-book'
 import {
   Chart,
   ArcElement,
@@ -17,23 +17,23 @@ Chart.register(
   Tooltip,
 );
 
-let ctx = document.querySelector("#graph").getContext("2d");
+const ctx = document.querySelector("#graph").getContext("2d");
 
 
-let gradient1 = ctx.createLinearGradient(0, 0, 0, 180)
+const gradient1 = ctx.createLinearGradient(0, 0, 0, 180)
 	gradient1.addColorStop(0.0, '#BC9CFF')
 	gradient1.addColorStop(1.0, '#8BA4F9');
-let gradient2 = ctx.createLinearGradient(0, 0, 0, 180)
+const gradient2 = ctx.createLinearGradient(0, 0, 0, 180)
   gradient2.addColorStop(0.0, '#6FCF97')
   gradient2.addColorStop(1.0, '#66D2EA');
-let gradient3 = ctx.createLinearGradient(0, 0, 0, 180)
+const gradient3 = ctx.createLinearGradient(0, 0, 0, 180)
   gradient3.addColorStop(0.0, '#FFE39C')
   gradient3.addColorStop(1.0, '#FFBA9C');
-let gradient4 = ctx.createLinearGradient(0, 0, 0, 180)
+const gradient4 = ctx.createLinearGradient(0, 0, 0, 180)
   gradient4.addColorStop(0.0, '#919191')
   gradient4.addColorStop(1.0, '#3D4975');
 
-new Chart(ctx, {
+const chart = new Chart(ctx, {
   type: 'doughnut',
   data: {
   	labels: [
