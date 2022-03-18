6e70412c85fe0ccfc5b939bb1c4271b5c8074261
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-03-18 18:02:01 +0300

    style: improved readability

--- src/index.js
+++ src/index.js
@@ -12,41 +12,44 @@ async function index() {
 	await import('./css/index.scss');
 	await import('./js/index.ts');
 }
-// async function searchRoom() {
-	// 	await import('./components/range-slider/range-slider.scss');
-	// 	await import('jquery-ui/themes/base/slider.css');
-// 	await import('./css/search-room.scss');
-// 	await import('./js/search-room.js');
-// }
-// async function roomDetails() {
-	// 	await import('jquery-ui/themes/base/datepicker.css');
-// 	await import('./css/room-details.scss');
-// 	await import('./js/room-details.js');
-// }
-// async function registration() {
-// 	await import('./css/registration.scss');
-// }
-// async function signIn() {
-// 	await import('./css/sign-in.scss');
-// }
-
 if ($('#index').length) {
 	index();
 }
-// if($('#search-room').length){
-// searchRoom()
-// }
-// if($('#room-details').length){
-// 	roomDetails()
-// }
-// if($('#registration').length){
-// 	registration()
-// }
-// if($('#sign-in').length){
-// 	signIn()
-// }
 
-function component() {
-	return;
+async function searchRoom() {
+	await import('./components/range-slider/range-slider.scss');
+	await import('jquery-ui/themes/base/slider.css');
+	await import('./css/search-room.scss');
+	await import('./js/search-room.js');
 }
-document.body.appendChild(component());
+if($('#search-room').length){
+	searchRoom()
+}
+
+async function roomDetails() {
+	await import('jquery-ui/themes/base/datepicker.css');
+	await import('./css/room-details.scss');
+	await import('./js/room-details.ts');
+}
+if($('#room-details').length){
+	roomDetails()
+}
+
+async function registration() {
+	await import('./css/registration.scss');
+}
+if($('#registration').length){
+	registration()
+}
+
+async function signIn() {
+	await import('./css/sign-in.scss');
+}
+if($('#sign-in').length){
+	signIn()
+}
+
+// function component() {
+// 	return;
+// }
+// document.body.appendChild(component());
