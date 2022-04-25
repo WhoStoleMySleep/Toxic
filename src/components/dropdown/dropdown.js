3e80443122cb93228eb03ed8d84e9d6d3fcb5b8a
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-04-26 02:00:17 +0700

    refactor(components): work on readability

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -5,22 +5,20 @@ $(document).ready(() => {
 	$('.dropdown').iqDropdown({
 		setSelectionText: (itemCount, totalItems) => { 
 			let totalLastNumber = +totalItems.toString().split('').pop()
-			let equalsZero = totalItems === 0
-
-			
+			let equalsZero = (totalItems === 0)
 
 			if(equalsZero){
 				return 'Сколько гостей'
 			}
-			const equalsOne = totalLastNumber === 1 && !(totalItems >= 11 && totalItems <= 14);
+			const equalsOne = (totalLastNumber === 1 && !(totalItems >= 11 && totalItems <= 14));
 			if(equalsOne) {
 				return `${totalItems} гость`
 			}
-			const twoToFour = totalLastNumber >= 2 && totalLastNumber <= 4 && !(totalItems >= 11 && totalItems <= 14)
+			const twoToFour = (totalLastNumber >= 2 && totalLastNumber <= 4 && !(totalItems >= 11 && totalItems <= 14))
 			if(twoToFour) {
 				return `${totalItems} гостя`
 			}
-			const allTheRest = totalLastNumber >= 5 && totalLastNumber <= 9 || totalItems >= 11 && totalItems <= 14
+			const allTheRest = (totalLastNumber >= 5 && totalLastNumber <= 9 || totalItems >= 11 && totalItems <= 14)
 			if(allTheRest) {
 				return `${totalItems} гостей`
 			}
