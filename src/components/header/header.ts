94be340eb05cea8408b1914965fb0f97a1098e36
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-05-11 01:21:47 +0700

    refactor(components): adding 'js-', indent by 2

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,14 +1,14 @@
 import compareElementTypes from '../../js/helper-modules/compareElementTypes';
 
-(function () {
-	const burgerButton = document.querySelector('.js-header__menu-burger');
-	const headerLinks = document.querySelector('.js-header__menu-links');
-	const body = document.body;
-	
-	if (!compareElementTypes(null, burgerButton, headerLinks)) {
-		burgerButton!.addEventListener('click', function() {
-			headerLinks!.classList.toggle('_active');
-			body!.classList.toggle('_blockSwipe');
-		});
-	}
+(() => {
+  const burgerButton = document.querySelector('.js-header__burger');
+  const headerLinks = document.querySelector('.js-header__link-list');
+  const body = document.querySelector('body');
+
+  if (!compareElementTypes(null, burgerButton, headerLinks)) {
+    burgerButton!.addEventListener('click', () => {
+      headerLinks!.classList.toggle('_active');
+      body!.classList.toggle('_blockSwipe');
+    });
+  }
 })();
