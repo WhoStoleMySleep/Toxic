da8797a06285a8edf57763606e8a7a0667d45d60
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-05-12 15:43:04 +0700

    style(components): code style compliance

--- src/components/like-button/like-button.ts
+++ src/components/like-button/like-button.ts
@@ -1,19 +1,19 @@
 const likeButton = document.querySelectorAll('.like-button');
 const likeButtonLiked = document.querySelectorAll('.like-button__liked');
 
-if(likeButton !== null && likeButtonLiked !== null){
-	for (let index = 0; index < likeButton.length; index += 1) {
-		likeButton[index].addEventListener('click', () => {
-			const likes = likeButtonLiked[index];
-			const likesVal = likes.innerHTML;
-	
-			if(likeButton[index].classList.contains('_deactive')){
-				likes.innerHTML = (+likesVal + 1).toString();
-			} else if(!likeButton[index].classList.contains('_deactive')) {
-				likes.innerHTML = (+likesVal - 1).toString();
-			}
-	
-			likeButton[index].classList.toggle('_deactive');
-		})
-	}
+if (likeButton !== null && likeButtonLiked !== null) {
+  for (let index = 0; index < likeButton.length; index += 1) {
+    likeButton[index].addEventListener('click', () => {
+      const likes = likeButtonLiked[index];
+      const likesVal = likes.innerHTML;
+
+      if (likeButton[index].classList.contains('_deactive')) {
+        likes.innerHTML = (+likesVal + 1).toString();
+      } else if (!likeButton[index].classList.contains('_deactive')) {
+        likes.innerHTML = (+likesVal - 1).toString();
+      }
+
+      likeButton[index].classList.toggle('_deactive');
+    });
+  }
 }
