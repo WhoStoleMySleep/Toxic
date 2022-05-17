2fcd4a5aa30b78ec0397b1bdd49f9dd509147856
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-05-17 14:01:32 +0700

    refactor(components): improved readability

---
+++ src/components/range-slider/range-slider.ts
@@ -0,0 +1,17 @@
+import 'jquery-ui/ui/widgets/slider';
+
+const amount: HTMLInputElement | null = document.querySelector('.js-range-slider__amount');
+const sliderOptions = {
+  range: true,
+  min: 0,
+  max: 15500,
+  values: [5000, 10000],
+  slide: (_: any, ui: any) => {
+    amount!.value = `${ui.values[0]}₽ - ${ui.values[1]}₽`;
+  },
+};
+
+$(document).ready(() => {
+  $('.js-range-slider__slider').slider(sliderOptions);
+  amount!.value = `${sliderOptions.values[0]}₽ - ${sliderOptions.values[1]}₽`;
+});
